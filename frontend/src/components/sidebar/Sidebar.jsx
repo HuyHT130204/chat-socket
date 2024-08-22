cfb7715accf7c6044e3fcd13b09a32b46323a912
John Doe <johndoe@email.com>
2024-08-23 00:02:21 +0700

    2nd part completed - ui design

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import Conversations from "./Conversations";
-import LogoutButton from "./LogoutButton";
+//import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
@@ -8,25 +8,8 @@ const Sidebar = () => {
 			<SearchInput />
 			<div className='divider px-3'></div>
 			<Conversations />
-			<LogoutButton />
+
 		</div>
 	);
 };
 export default Sidebar;
-
-// STARTER CODE FOR THIS FILE
-// import Conversations from "./Conversations";
-// import LogoutButton from "./LogoutButton";
-// import SearchInput from "./SearchInput";
-
-// const Sidebar = () => {
-// 	return (
-// 		<div className='border-r border-slate-500 p-4 flex flex-col'>
-// 			<SearchInput />
-// 			<div className='divider px-3'></div>
-// 			<Conversations />
-// 			<LogoutButton />
-// 		</div>
-// 	);
-// };
-// export default Sidebar;
